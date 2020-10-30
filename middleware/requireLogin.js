5c5b69791b4bca70fdd800396da63a0e144de852
thayan funny <thayan.siva@gmail.com>
2020-10-30 00:56:44 -0400

    make it better

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebToken')
+const jwt = require('jsonwebtoken')
 const {JWT_SECRET}=require('../config/keys')
 const mongoose = require('mongoose')
 const User= mongoose.model("User")
