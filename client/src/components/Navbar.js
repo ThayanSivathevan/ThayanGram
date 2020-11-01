26357b893862c422fe667ec9df198f1a0de044cb
thayan funny <thayan.siva@gmail.com>
2020-11-01 11:44:27 -0500

    make it better

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,103 +1,96 @@
-import React, { useContext, useRef, useEffect, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import { UserContext } from '../App'
+import React,{useContext,useRef,useEffect,useState} from 'react'
+import {Link ,useHistory} from 'react-router-dom'
+import {UserContext} from '../App'
 import M from 'materialize-css'
-import { set } from 'mongoose'
-const Navbar = () => {
-  const searchModal = useRef(null)
-  const [search, setSearch] = useState('')
-  const [userDetails, setUserDetails] = useState([])
-  const { state, dispatch } = useContext(UserContext)
-  const history = useHistory();
-  useEffect(() => {
-    M.Modal.init(searchModal.current)
-    fetchUsers("")
-  }, [])
-  const renderList = () => {
-    if (state) {
-      return [
-        <div key="1" >
-          <li ><i data-target="modal1" className="large material-icons modal-trigger" style={{ color: "black" }}>search</i></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/create">Create Post</Link></li>
-          <li><Link to="/myHome">My home</Link></li>
-          <li><Link to="/login">
-            <button className="btn waves-effect waves-light #4a148c purple darken-4"
-              onClick={() => {
-                localStorage.clear()
-                dispatch({ type: "CLEAR" })
-                M.toast({ html: "Logged out", classes: "#4a148c purple darken-4" })
-                history.push('/')
-              }}>
-              Log out
-                </button>
-          </Link></li>
-          {renderModal()}
-        </div>
-      ]
-    }
-    else {
-      return [
-        <div key="2">
-          <li ><Link to="/login">Log in</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-        </div>
-      ]
-    }
-  }
-  const renderHome = () => {
+const NavBar = ()=>{
+    const  searchModal = useRef(null)
+    const [search,setSearch] = useState('')
+    const [userDetails,setUserDetails] = useState([])
+     const {state,dispatch} = useContext(UserContext)
+     const history = useHistory()
+     useEffect(()=>{
+         M.Modal.init(searchModal.current)
+     },[])
+     const renderList = ()=>{
+       if(state){
+           return [
+            <li key="1"><i  data-target="modal1" className="large material-icons modal-trigger" style={{color:"black"}}>search</i></li>,
+            <li key="2"><Link to="/profile">Profile</Link></li>,
+            <li key="3"><Link to="/create">Create Post</Link></li>,
+            <li key="4"><Link to="/myHome">My following Posts</Link></li>,
+            <li  key="5">
+             <button className="btn #c62828 red darken-3"
+            onClick={()=>{
+              localStorage.clear()
+              dispatch({type:"CLEAR"})
+              history.push('/login')
+            }}
+            >
+                Logout
+            </button>
+            </li>
+         
+            
+           ]
+       }else{
+         return [
+          <li  key="6"><Link to="/login">Signin</Link></li>,
+          <li  key="7"><Link to="/signup">Signup</Link></li>
+         
+         ]
+       }
+     }
 
-    return [<Link to={state ? "/" : "/login"} className="brand-logo left">ThayanGram</Link>]
 
-  }
-  const renderModal=()=>{
-    return [<div id="modal1" className="modal" ref={searchModal} style={{ color: "black" }}>
-          <div className="modal-content input-field">
-            <input value={search} onChange={(e) => fetchUsers(e.target.value)} type="text" placeholder="search" />
-          <ul className="collection">
-            {userDetails.map(item => {
-              return <Link to={item._id !== state._id ? "/middle/" + item._id : "/profile"} 
-              onClick={()=>{
-                M.Modal.getInstance(searchModal.current).close()
-                setSearch('')}
-              }><li className="collection-item">{item.name + " " + item.email}</li></Link>
-            })}
+     const fetchUsers = (query)=>{
+        setSearch(query)
+        fetch('/search-users',{
+          method:"post",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({
+            query
+          })
+        }).then(res=>res.json())
+        .then(results=>{
+          setUserDetails(results.user)
+        })
+     }
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
+          <ul id="nav-mobile" className="right">
+             {renderList()}
+  
           </ul>
-
         </div>
-        <div className="modal-footer">
-          <button className="modal-close waves-effect waves-green btn-flat" onClick={()=>setSearch('')}>Close</button>
+        <div id="modal1" class="modal" ref={searchModal} style={{color:"black"}}>
+          <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e)=>fetchUsers(e.target.value)}
+            />
+             <ul className="collection">
+               {userDetails.map(item=>{
+                 return <Link to={item._id !== state._id ? "/profile/"+item._id:'/profile'} onClick={()=>{
+                   M.Modal.getInstance(searchModal.current).close()
+                   setSearch('')
+                 }}><li className="collection-item">{item.email}</li></Link> 
+               })}
+               
+              </ul>
+          </div>
+          <div className="modal-footer">
+            <button className="modal-close waves-effect waves-green btn-flat" onClick={()=>setSearch('')}>close</button>
+          </div>
         </div>
-      </div>]
-  }
-
-
-  const fetchUsers = (query) => {
-    setSearch(query)
-    fetch('/search-users', {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("jwt")
-      },
-      body: JSON.stringify({
-        query
-      })
-    }).then(res => res.json()).then(results => {
-      setUserDetails(results.user)
-    })
-  }
-
-  return (
-    <nav>
-      <div className="nav-wrapper white">
-        {renderHome()}
-        <ul id="nav-mobile" className="right">
-          {renderList()}
-        </ul>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
 }
 
-export default Navbar
+
+export default NavBar
